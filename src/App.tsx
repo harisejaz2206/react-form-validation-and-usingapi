e312956e4e2a3974be06ec949258bcf87353094d
harisejaz2206 <harisejaz2206@gmail.com>
2023-08-09 11:00:29 +0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-center font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
